fd0ab84d27ed6d04b64e804f44d82c4dae3d6104
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-08-12 20:59:33 +0500

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:envirosense_fyp/MainPage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
       RestartWidget(
         child: MaterialApp(
